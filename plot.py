e072b143031ce4e89029b6604d0a88662909237f
Howon Lee <howon@howonlee.com>
2015-01-18 10:44:42 -0800

    begin plotting

---
+++ plot.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import h5py
+
+f = h5py.File("mhd1024.h5", "r")
+total_array = np.array([0,0,0])
+for name in f:
+    last_arr = np.squeeze(f[name][...])
+    print last_arr
+    if last_arr.shape == (3,):
+        total_array = np.vstack((total_array, last_arr))
+print "====="
+print total_array
+one_col = total_array[:,1]
+plt.plot(one_col)
+plt.show()
