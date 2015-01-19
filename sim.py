775cd11ebb946fc83ca28f9c1e514bd8c0575bfd
Howon Lee <howon@howonlee.com>
2015-01-19 00:26:07 -0800

    more experimentation

---
+++ sim.py
@@ -0,0 +1,16 @@
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def calc_autocorrelation(fbms):
+    pass
+
+def gen_one_fbm():
+    pass
+
+def gen_fbms(num=3000):
+    fbms = np.zeros(1)
+    return fbms
+
+if __name__ == "__main__":
+    pass
