f8b2edae72692d91dd1ed3bc089f75573a64e45f
Howon Lee <howon@howonlee.com>
2015-01-21 15:37:35 -0800

    no more old plot

--- plot.py
+++ plot.py
@@ -1,16 +1,10 @@
-import numpy as np
 import matplotlib.pyplot as plt
-import h5py
 
-f = h5py.File("isotropic1024fine.h5", "r")
-total_array = np.array([0,0,0])
-for name in f:
-    last_arr = np.squeeze(f[name][...])
-    print last_arr
-    if last_arr.shape == (3,):
-        total_array = np.vstack((total_array, last_arr))
-print "====="
-print total_array
-one_col = total_array[:,1]
-plt.plot(one_col)
-plt.show()
+if __name__ == "__main__":
+    turb_data = []
+    with open("turb.dat", "r") as turb_file:
+        turb_data = turb_file.read().split()
+    turb_data = map(int, turb_data)
+    #print turb_data[0]
+    plt.plot(turb_data)
+    plt.show()
