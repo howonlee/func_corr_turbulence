cae234c067179f1f8f61333b77b1bbfee478ab93
Howon Lee <howon@howonlee.com>
2015-01-21 15:39:37 -0800

    turbulence retmap seems pretty fbm-ish to mee..

--- plot.py
+++ plot.py
@@ -1,10 +1,19 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
+def return_map(vals, num_pts=1000):
+    vals = vals[:num_pts]
+    ret_mat = np.zeros((num_pts, num_pts))
+    #probably a vectorized way to do it
+    for x in xrange(num_pts):
+        for y in xrange(num_pts):
+            ret_mat[x,y] = abs(vals[x] - vals[y])
+    plt.matshow(ret_mat)
+    plt.show()
+    plt.savefig("turb_retmap")
 
 if __name__ == "__main__":
     turb_data = []
     with open("turb.dat", "r") as turb_file:
-        turb_data = turb_file.read().split()
-    turb_data = map(int, turb_data)
-    #print turb_data[0]
-    plt.plot(turb_data)
-    plt.show()
+        turb_data = map(int, turb_file.read().split())
+    return_map(turb_data)
